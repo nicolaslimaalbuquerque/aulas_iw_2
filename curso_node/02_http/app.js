61b935d9dd71ca314e958d73631aceb52135db90
Nicolas Lima Albuquerque <124801443+nicolaslimaalbuquerque@users.noreply.github.com>
2024-06-06 09:02:48 -0300

    Delete curso_node directory

--- curso_node/02_http/app.js
+++
@@ -1,9 +0,0 @@
-//solicitar um modulo interno do node chamado http
-var http = require('http')
-
-//abrindo um servidor http
-http.createServer(function(req,res){
-    res.end("OII ETEC MCM SEUS LINDOS")
-}).listen(8081)
-
-console.log('servidor rodando...')
